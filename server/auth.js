bdbd1836152633c160c683497368f518eafa206d
Dan Goodman <dan@danthegoodman.com>
2020-05-06 14:04:33 -0400

    integrated into DO api

--- server/auth.js
+++ server/auth.js
@@ -2,6 +2,7 @@ const exec = require('child_process').exec
 const fs = require('fs')
 const path = require('path')
 const { v4: uuidv4 } = require('uuid')
+const Do = require('do-wrapper').default
 
 exports.generateCerts = () => {
   return new Promise((resolve, reject) => {
@@ -29,3 +30,17 @@ if (!fs.existsSync(path.join(__dirname, 'jwtsecret'))) {
 }
 
 exports.jwtsecret = jwtsecret
+
+let dokey
+let api
+
+if (!fs.existsSync(path.join(__dirname, 'dokey'))) {
+  console.log('DigitalOcean key not found. Please Create a file called "dokey" and paste in the contents of a Read/Write API Key')
+  process.exit()
+} else {
+  dokey = fs.readFileSync(path.join(__dirname, 'dokey'), 'utf-8').replace(/(\r\n|\n|\r)/gm, '')
+  api = new Do(dokey)
+}
+
+exports.dokey = dokey
+exports.doAPI = api
--- server/manager.js
+++ server/manager.js
@@ -1,4 +1,5 @@
 const { ws } = require('./http')
+const { doAPI } = require('./auth')
 
 ws.on('connection', (socket, req) => {
   socket.on('message', async (msg) => {
@@ -6,3 +7,30 @@ ws.on('connection', (socket, req) => {
     socket.send('Thanks!')
   })
 })
+
+let projectID = '03ad5a9b-6213-46a3-976a-fb5605d5ee1a' // TODO: Unhardcode
+
+let agents = {} // List of agents
+
+// Test API Access
+// doAPI.projects.getDefault()
+//   .then((res) => {
+//     if (process.env.DEBUG) {
+//       console.log(res)
+//     }
+//     // if (res.id) {
+//     //   projectID = res.id
+//     // }
+//     console.log(`Found default project: '${res.name}'`)
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
+
+doAPI.projects.getById('03ad5a9b-6213-46a3-976a-fb5605d5ee1a')
+  .then((res) => {
+    console.log(res)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
