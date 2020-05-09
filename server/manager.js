d4f7ce823bf97aef0177e164704994ee1814cb78
Dan Goodman <dan@danthegoodman.com>
2020-05-09 15:44:12 -0400

    create droplet

--- server/manager.js
+++ server/manager.js
@@ -2,7 +2,27 @@ const { ws } = require('./http')
 const { doAPI } = require('./auth')
 
 let agents = {} // List of agents // TODO: Make into a db style with something like level
-let projectID = '03ad5a9b-6213-46a3-976a-fb5605d5ee1a' // TODO: Unhardcode
+let projectID = '6f934c4c-f400-4a4f-b326-00a50dd39a37' // TODO: Unhardcode
+let instanceModel = {
+
+}
+
+const dropletSizes = [
+  "s-1vcpu-1gb",
+  "s-1vcpu-2gb",
+  "s-1vcpu-3gb",
+  "s-2vcpu-2gb",
+  "s-3vcpu-1gb",
+  "s-2vcpu-4gb",
+  "s-4vcpu-8gb",
+  "s-6vcpu-16gb",
+  "s-8vcpu-32gb",
+  "s-12vcpu-48gb",
+  "s-16vcpu-64gb",
+  "s-20vcpu-96gb",
+  "s-24vcpu-128gb",
+  "s-32vcpu-192gb"
+]
 
 if (process.env.DEBUG) {
   setInterval(() => {
@@ -29,7 +49,7 @@ setInterval(() => { // Scaler monitor
   } else {
     monitorCycles = 0
   }
-  if (monitorCycles > 3) { // 3 Checkins
+  if (monitorCycles > 1 && !scalingLock) { // 3 Checkins
     console.log('Scaling...')
     scalingLock = true
     scaleUp()
@@ -64,7 +84,23 @@ ws.on('connection', (socket, req) => {
   })
 })
 
-doAPI.projects.getById(projectID)
+// doAPI.projects.getById(projectID)
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
+
+// doAPI.images.getByIdOrSlug('ubuntu-18-04-x64')
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
+
+doAPI.keys.getAll()
   .then((res) => {
     console.log(res)
   })
@@ -72,10 +108,43 @@ doAPI.projects.getById(projectID)
     console.error(err)
   })
 
+const createDroplet = (image, name, region, dropletSize, sshKeys) => {
+  doAPI.droplets.create({
+    "name": name,
+    "region": region,
+    "size": dropletSize,
+    "image": image, // "ubuntu-18-04-x64"
+    "ssh_keys": sshKeys,
+    "backups": false,
+    "ipv6": false,
+    "user_data": null,
+    "private_networking": null,
+    "volumes": null,
+    "tags": []
+  })
+    .then((data) => {
+      // console.log(data)
+      console.log('Created droplet...')
+      doAPI.projects.addResources(projectID, [`do:droplet:${data.droplet.id}`])
+        .then((res) => {
+          // console.log(res)
+          console.log('Assigned droplet to project!')
+          scalingLock = false
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
 const scaleUp = () => {
   console.log('Scaling up!')
   // Fetch information about what instance to create
   // Create instance, get instance name
   // Check for instance checkin
   // Remove scaling locks
+  createDroplet('ubuntu-18-04-x64', 'testdroplet', 'nyc3', dropletSizes[0], 20081905)
 }
